2ae3d131eaeac6ffcce199d23a07927d3e06babe
valik711 <androvalik@gmail.com>
2018-04-14 11:00:28 +0300

    initial commit

--- app/src/main/java/com/valentinfilatov/wober/MainActivity.java
+++ app/src/main/java/com/valentinfilatov/wober/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         navigationView.setNavigationItemSelectedListener(this);
     }
 
