2dadc0547b6805a84f34c1ad49319c81e6b3fde9
valik711 <androvalik@gmail.com>
2018-04-18 16:52:52 +0300

    updated main screen as master

--- app/src/main/java/com/valentinfilatov/wober/MainActivity.java
+++ app/src/main/java/com/valentinfilatov/wober/MainActivity.java
@@ -11,8 +11,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
